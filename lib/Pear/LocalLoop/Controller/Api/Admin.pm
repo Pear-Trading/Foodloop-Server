b48edea9fb7de1679f8d04d6656741b335a6ab4a
Tom Bloor <t.bloor@shadowcat.co.uk>
2017-04-21 14:54:19 +0100

    Fixed admin-merge test after refactoring

--- lib/Pear/LocalLoop/Controller/Api/Admin.pm
+++ lib/Pear/LocalLoop/Controller/Api/Admin.pm
@@ -3,6 +3,11 @@ use Mojo::Base 'Mojolicious::Controller';
 
 has error_messages => sub {
   return {
+    target_organisation_id => {
+      required => { message => 'target_organisation_id is missing', status => 400 },
+      number => { message => 'target_organisation_id is not a number', status => 400 },
+      in_resultset => { message => 'target_organisation_id does not exist in the database', status => 400 },
+    },
     pending_organisation_id => {
       required => { message => 'pending_organisation_id is missing', status => 400 },
       number => { message => 'pending_organisation_id is not a number', status => 400 },
@@ -33,7 +38,6 @@ sub auth {
 
 sub post_admin_approve {
   my $c = shift;
-  my $self = $c;
 
   my $validation = $c->validation;
 
@@ -57,22 +61,9 @@ sub post_admin_approve {
     postcode    => $validation->param('postcode') || $pending_org->postcode,
   });
 
-  my $pending_transaction_rs = $pending_org->pending_transactions;
+  $c->copy_transactions_and_delete( $pending_org, $valid_org );
 
-  while ( my $pending_transaction = $pending_transaction_rs->next ) {
-    $valid_org->create_related(
-      'transactions', {
-        buyeruserid_fk     => $pending_transaction->buyeruserid_fk,
-        valuemicrocurrency => $pending_transaction->valuemicrocurrency,
-        proofimage         => $pending_transaction->proofimage,
-        timedatesubmitted  => $pending_transaction->timedatesubmitted,
-      }
-    );
-  }
-
-  $pending_org->delete;
-
-  return $self->render(
+  return $c->render(
     json => {
       success => Mojo::JSON->true,
       message => 'Successfully Approved Organisation',
@@ -83,111 +74,53 @@ sub post_admin_approve {
 
 
 sub post_admin_merge {
-  my $self = shift;
+  my $c = shift;
 
-  my $userId = $self->get_active_user_id();
-  if ( ! $self->is_admin($userId) ) {
-    $self->app->log->debug('Path Error: file:' . __FILE__ . ', line: ' . __LINE__);
-    return $self->render( json => {
-      success => Mojo::JSON->false,
-      message => 'You are not an admin.',
-    },
-    status => 403,); #Forbidden request   
-  }
+  my $validation = $c->validation;
 
-  my $json = $self->req->json;
-  if ( ! defined $json ) {
-    $self->app->log->debug('Path Error: file:' . __FILE__ . ', line: ' . __LINE__);
-    return $self->render( json => {
-      success => Mojo::JSON->false,
-      message => 'JSON is missing.',
-    },
-    status => 400,); #Malformed request   
-  }
+  $validation->input( $c->stash->{api_json} );
 
-  my $unvalidatedOrganisationId = $json->{unvalidatedOrganisationId};
-  if ( ! defined $unvalidatedOrganisationId ) {
-    $self->app->log->debug('Path Error: file:' . __FILE__ . ', line: ' . __LINE__);
-    return $self->render( json => {
-      success => Mojo::JSON->false,
-      message => 'unvalidatedOrganisationId is missing.',
-    },
-    status => 400,); #Malformed request   
-  }
-  elsif (! Scalar::Util::looks_like_number($unvalidatedOrganisationId)){
-    $self->app->log->debug('Path Error: file:' . __FILE__ . ', line: ' . __LINE__);
-    return $self->render( json => {
-      success => Mojo::JSON->false,
-      message => 'unvalidatedOrganisationId does not look like a number.',
-    },
-    status => 400,); #Malformed request   
-  }
+  my $pending_org_rs = $c->schema->resultset('PendingOrganisation');
+  $validation->required('pending_organisation_id')->number->in_resultset('id', $pending_org_rs);
 
-  my $validatedOrganisationId = $json->{validatedOrganisationId};
-  if ( ! defined $validatedOrganisationId ) {
-    $self->app->log->debug('Path Error: file:' . __FILE__ . ', line: ' . __LINE__);
-    return $self->render( json => {
-      success => Mojo::JSON->false,
-      message => 'validatedOrganisationId is missing.',
-    },
-    status => 400,); #Malformed request   
-  }
-  elsif (! Scalar::Util::looks_like_number($validatedOrganisationId)){
-    $self->app->log->debug('Path Error: file:' . __FILE__ . ', line: ' . __LINE__);
-    return $self->render( json => {
-      success => Mojo::JSON->false,
-      message => 'validatedOrganisationId does not look like a number.',
-    },
-    status => 400,); #Malformed request   
-  }
+  my $valid_org_rs = $c->schema->resultset('Organisation');
+  $validation->required('target_organisation_id')->number->in_resultset('id', $valid_org_rs);
 
+  return $c->api_validation_error if $validation->has_error;
+
+  my $pending_org = $pending_org_rs->find( $validation->param('pending_organisation_id') );
+  my $target_org = $valid_org_rs->find( $validation->param('target_organisation_id') );
+ 
+  $c->copy_transactions_and_delete( $pending_org, $target_org );
   #FIXME This requires mutual exclusion.
 
-  my $doesUnvalidatedIdNotExist = ($self->db->selectrow_array("SELECT COUNT(*) FROM PendingOrganisations WHERE PendingOrganisationId = ?", undef, ($unvalidatedOrganisationId)) == 0);
-  if ($doesUnvalidatedIdNotExist) {
-    $self->app->log->debug('Path Error: file:' . __FILE__ . ', line: ' . __LINE__);
-    return $self->render( json => {
-      success => Mojo::JSON->false,
-      message => 'unvalidatedOrganisationId does not exist in the database.',
+  return $c->render(
+    json => {
+      success => Mojo::JSON->true,
+      message => 'Successfully Merged Organisations',
     },
-    status => 400,); #Malformed request 
-  }
-
-  my $doesValidatedIdNotExist = ($self->db->selectrow_array("SELECT COUNT(*) FROM Organisations WHERE OrganisationalId = ?", undef, ($validatedOrganisationId)) == 0);
-  if ($doesValidatedIdNotExist) {
-    $self->app->log->debug('Path Error: file:' . __FILE__ . ', line: ' . __LINE__);
-    return $self->render( json => {
-      success => Mojo::JSON->false,
-      message => 'validatedOrganisationId does not exist in the database.',
-    },
-    status => 400,); #Malformed request 
-  }
-  
-
-  #FIXME there may be race conditions here, so may get the wrong number, mutux is needed.
-  my $statementSelectPendingTrans = $self->db->prepare("SELECT BuyerUserId_FK, ValueMicroCurrency, ProofImage, TimeDateSubmitted FROM PendingTransactions WHERE PendingSellerOrganisationId_FK = ?");
-  $statementSelectPendingTrans->execute($unvalidatedOrganisationId);
-
-  my $statementInsTrans = $self->db->prepare("INSERT INTO Transactions (BuyerUserId_FK, SellerOrganisationId_FK, ValueMicroCurrency, ProofImage, TimeDateSubmitted) VALUES (?, ?, ?, ?, ?)");
-
-  #Move all transactions from pending onto verified.
-  while (my ($buyerUserId, $value, $imgName, $timeDate) = $statementSelectPendingTrans->fetchrow_array()) {
-    $statementInsTrans->execute($buyerUserId, $validatedOrganisationId, $value, $imgName, $timeDate);
-  }
-
-  #Delete transactions first, so there is no dependancy when deleting the row from PendingOrganisations.
-  $self->db->prepare("DELETE FROM PendingTransactions WHERE PendingSellerOrganisationId_FK = ?")->execute($unvalidatedOrganisationId);
-  $self->db->prepare("DELETE FROM PendingOrganisations WHERE PendingOrganisationId = ?")->execute($unvalidatedOrganisationId);
-
-  $self->app->log->debug('Path Success: file:' . __FILE__ . ', line: ' . __LINE__);
-  return $self->render( json => {
-    success => Mojo::JSON->true,
-  },
-  status => 200,);  
-
+    status => 200,
+  );  
 }
 
+sub copy_transactions_and_delete {
+  my ( $c, $from_org, $to_org ) = @_;
 
+  my $from_org_transaction_rs = $from_org->pending_transactions;
+
+  while ( my $from_org_transaction = $from_org_transaction_rs->next ) {
+    $to_org->create_related(
+      'transactions', {
+        buyeruserid_fk     => $from_org_transaction->buyeruserid_fk,
+        valuemicrocurrency => $from_org_transaction->valuemicrocurrency,
+        proofimage         => $from_org_transaction->proofimage,
+        timedatesubmitted  => $from_org_transaction->timedatesubmitted,
+      }
+    );
+  }
+
+  $from_org->delete;
+}
 
 1;
 
