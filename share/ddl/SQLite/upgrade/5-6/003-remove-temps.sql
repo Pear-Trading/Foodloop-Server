b90534babba051a052ab0ebda76334964f698891
Tom Bloor <t.bloor@shadowcat.co.uk>
2017-09-06 16:55:05 +0100

    Fixed table clearup sql for migration

--- share/ddl/SQLite/upgrade/5-6/003-remove-temps.sql
+++ share/ddl/SQLite/upgrade/5-6/003-remove-temps.sql
@@ -6,6 +6,8 @@ DROP TABLE customers_temp;
 DROP TABLE organisations_temp;
 DROP TABLE transactions_temp;
 DROP TABLE users_temp;
+DROP TABLE feedback_temp;
+DROP TABLE session_tokens_temp;
 DROP TABLE pending_organisations;
 DROP TABLE pending_transactions;
 DROP TABLE administrators;
