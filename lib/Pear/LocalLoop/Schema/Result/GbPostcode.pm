5a80d57ca2a14f7985a3d00e84bd0d5b45728178
Tom Bloor <t.bloor@shadowcat.co.uk>
2017-09-21 15:48:25 +0100

    Added table for storing postcodes

---
+++ lib/Pear/LocalLoop/Schema/Result/GbPostcode.pm
@@ -0,0 +1,38 @@
+package Pear::LocalLoop::Schema::Result::GbPostcode;
+
+use strict;
+use warnings;
+
+use base 'DBIx::Class::Core';
+
+__PACKAGE__->table('gb_postcodes');
+
+__PACKAGE__->add_columns(
+  outcode => {
+    data_type => 'char',
+    size => 4,
+    is_nullable => 0,
+  },
+  incode => {
+    data_type => 'char',
+    size => 3,
+    is_nullable => 0,
+    default_value => '',
+  },
+  latitude => {
+    data_type => 'decimal',
+    size => [7,5],
+    is_nullable => 1,
+    default_value => undef,
+  },
+  longitude => {
+    data_type => 'decimal',
+    size => [7,5],
+    is_nullable => 1,
+    default_value => undef,
+  },
+);
+
+__PACKAGE__->set_primary_key(qw/ outcode incode /);
+
+1;
