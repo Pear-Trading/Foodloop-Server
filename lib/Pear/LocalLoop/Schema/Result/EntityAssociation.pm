f5b94098fb7d26bd2766941cf8f938cdf8b72aa0
Finn <toyou1995@gmail.com>
2017-11-23 13:20:26 +0000

    Entity Assoiciations added to database

---
+++ lib/Pear/LocalLoop/Schema/Result/EntityAssociation.pm
@@ -0,0 +1,39 @@
+package Pear::LocalLoop::Schema::Result::EntityAssociation;
+
+use strict;
+use warnings;
+
+use base 'DBIx::Class::Core';
+
+__PACKAGE__->load_components(qw/
+  InflateColumn::DateTime
+  TimeStamp
+/);
+
+__PACKAGE__->table("entity_association");
+
+__PACKAGE__->add_columns(
+  "id" => {
+    data_type => "integer",
+    is_auto_increment => 1,
+    is_nullable => 0,
+  },
+  "entity_id" => {
+    data_type => 'integer',
+    is_nullable => 0,
+    is_foreign_key => 1,
+  },
+  "lis" => {
+    data_type => 'boolean',
+    default => undef,
+    is_nullable => 1,
+  },
+);
+
+__PACKAGE__->set_primary_key("id");
+
+__PACKAGE__->belongs_to(
+  "entity",
+  "Pear::LocalLoop::Schema::Result::Entity",
+  "entity_id",
+);
