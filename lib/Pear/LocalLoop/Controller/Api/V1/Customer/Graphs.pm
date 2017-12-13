8067d3cb9689a60fc8e96d987da1bd9007bfba99
Finn <toyou1995@gmail.com>
2017-12-13 13:33:29 +0000

    made customer graphs work with passing test

--- lib/Pear/LocalLoop/Controller/Api/V1/Customer/Graphs.pm
+++ lib/Pear/LocalLoop/Controller/Api/V1/Customer/Graphs.pm
@@ -16,14 +16,10 @@ sub index {
   my $validation = $c->validation;
   $validation->input( $c->stash->{api_json} );
   $validation->required('graph')->in( qw/
-    total_today
-    avg_spend_today
     total_last_week
     avg_spend_last_week
     total_last_month
     avg_spend_last_month
-    total_user
-    avg_spend_user
   / );
 
   return $c->api_validation_error if $validation->has_error;
@@ -45,6 +41,9 @@ sub index {
   return $c->$graph_sub;
 }
 
+
+# Replace with code for doughnut
+=pod
 sub graph_customers_range {
   my $c = shift;
 
@@ -78,87 +77,14 @@ sub graph_customers_range {
     }
   );
 }
+=cut
 
-sub graph_customers_last_7_days {
-  my $c = shift;
+sub graph_total_last_week { return shift->_purchases_total_duration( 7 ) }
+sub graph_total_last_month { return shift->_purchases_total_duration( 30 ) }
 
-  my $duration = DateTime::Duration->new( days => 7 );
-  return $c->_customers_last_duration( $duration );
-}
-
-sub graph_customers_last_30_days {
-  my $c = shift;
-
-  my $duration = DateTime::Duration->new( days => 30 );
-  return $c->_customers_last_duration( $duration );
-}
-
-sub _customers_last_duration {
+sub _purchases_total_duration {
   my ( $c, $duration ) = @_;
 
-  my $entity = $c->stash->{api_user}->entity;
-
-  my $data = { labels => [], data => [] };
-
-  my ( $start, $end ) = $c->_get_start_end_duration( $duration );
-
-  while ( $start < $end ) {
-    my $next_end = $start->clone->add( days => 1 );
-    my $transactions = $entity->sales
-      ->search_between( $start, $next_end )
-      ->count;
-    push @{ $data->{ labels } }, $start->day_name;
-    push @{ $data->{ data } }, $transactions;
-    $start->add( days => 1 );
-  }
-
-  return $c->render(
-    json => {
-      success => Mojo::JSON->true,
-      graph => $data,
-    }
-  );
-}
-
-sub graph_sales_last_7_days { return shift->_sales_last_duration( 7 ) }
-sub graph_sales_last_30_days { return shift->_sales_last_duration( 30 ) }
-
-sub _sales_last_duration {
-  my ( $c, $day_duration ) = @_;
-
-  my $duration = DateTime::Duration->new( days => $day_duration );
-  my $entity = $c->stash->{api_user}->entity;
-
-  my $data = { labels => [], data => [] };
-
-  my ( $start, $end ) = $c->_get_start_end_duration( $duration );
-
-  while ( $start < $end ) {
-    my $next_end = $start->clone->add( days => 1 );
-    my $transactions = $entity->sales
-      ->search_between( $start, $next_end )
-      ->get_column('value')
-      ->sum || 0 + 0;
-    push @{ $data->{ labels } }, $start->day_name;
-    push @{ $data->{ data } }, $transactions / 100000;
-    $start->add( days => 1 );
-  }
-
-  return $c->render(
-    json => {
-      success => Mojo::JSON->true,
-      graph => $data,
-    }
-  );
-}
-
-sub graph_purchases_last_7_days { return shift->_purchases_last_duration( 7 ) }
-sub graph_purchases_last_30_days { return shift->_purchases_last_duration( 30 ) }
-
-sub _purchases_last_duration {
-  my ( $c, $day_duration ) = @_;
-
-  my $duration = DateTime::Duration->new( days => $day_duration );
   my $entity = $c->stash->{api_user}->entity;
 
   my $data = { labels => [], data => [] };
@@ -184,11 +110,89 @@ sub _purchases_last_duration {
   );
 }
 
+sub graph_avg_spend_last_week { return shift->_purchases_avg_spend_duration( 7 ) }
+sub graph_avg_spend_last_month { return shift->_purchases_avg_spend_duration( 30 ) }
+
+sub _purchases_avg_spend_duration {
+  my ( $c, $day_duration ) = @_;
+
+  my $duration = DateTime::Duration->new( days => $day_duration );
+  my $entity = $c->stash->{api_user}->entity;
+
+  my $data = { labels => [], data => [] };
+
+  my ( $start, $end ) = $c->_get_start_end_duration( $duration );
+
+  my $dtf = $c->schema->storage->datetime_parser;
+  my $driver = $c->schema->storage->dbh->{Driver}->{Name};
+  my $transaction_rs = $c->schema->resultset('ViewQuantisedTransaction' . $driver)->search(
+    {
+      purchase_time => {
+        -between => [
+          $dtf->format_datetime($start),
+          $dtf->format_datetime($end),
+        ],
+      },
+    },
+    {
+      columns => [
+        {
+          quantised        => 'quantised_days',
+          count            => \"COUNT(*)",
+          sum_value        => $c->pg_or_sqlite(
+                                'SUM("me"."value")',
+                                'SUM("me"."value")',
+                              ),
+          average_value    => $c->pg_or_sqlite(
+                                'AVG("me"."value")',
+                                'AVG("me"."value")',
+                              ),
+        }
+      ],
+      group_by => 'quantised_days',
+      order_by => { '-asc' => 'quantised_days' },
+    }
+  );
+
+my $data = {
+  labels => [],
+  data => [],
+};
+
+  for ( $transaction_rs->all ) {
+    my $quantised = $c->db_datetime_parser->parse_datetime($_->get_column('quantised'));
+    push @{ $data->{ labels } }, $quantised->day_name;
+    push @{ $data->{ data } }, ($_->get_column('average_value') || 0) / 100000;
+  }
+
+  return $c->render(
+    json => {
+      success => Mojo::JSON->true,
+      graph => $data,
+    }
+  );
+}
+
 sub _get_start_end_duration {
   my ( $c, $duration ) = @_;
   my $end = DateTime->today;
   my $start = $end->clone->subtract_duration( $duration );
   return ( $start, $end );
+}
+
+sub pg_or_sqlite {
+  my ( $c, $pg_sql, $sqlite_sql ) = @_;
+
+  my $driver = $c->schema->storage->dbh->{Driver}->{Name};
+
+  if ( $driver eq 'Pg' ) {
+    return \$pg_sql;
+  } elsif ( $driver eq 'SQLite' ) {
+    return \$sqlite_sql;
+  } else {
+    $c->app->log->warn('Unknown Driver Used');
+    return undef;
+  }
 }
 
 1;
