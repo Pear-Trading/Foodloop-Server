b3832845193e94a49656ce31ad66c03d1a345f4c
Finn <toyou1995@gmail.com>
2018-01-17 16:47:05 +0000

    Added initial ability to get transaction category list from month

---
+++ lib/Pear/LocalLoop/Schema/Result/ViewQuantisedTransactionCategorySQLite.pm
@@ -0,0 +1,26 @@
+package Pear::LocalLoop::Schema::Result::ViewQuantisedTransactionCategorySQLite;
+
+use strict;
+use warnings;
+
+use base 'DBIx::Class::Core';
+
+__PACKAGE__->table_class('DBIx::Class::ResultSource::View');
+__PACKAGE__->table('view_quantised_transactions');
+__PACKAGE__->result_source_instance->is_virtual(1);
+
+__PACKAGE__->result_source_instance->view_definition( qq/
+SELECT "value",
+       "distance",
+       "purchase_time",
+       "buyer_id",
+       "seller_id",
+       "transaction_category.category_id",
+       DATETIME(STRFTIME('%Y-%m-%d %H:00:00',"purchase_time")) AS "quantised_hours",
+       DATETIME(STRFTIME('%Y-%m-%d 00:00:00',"purchase_time")) AS "quantised_days",
+       DATETIME(STRFTIME('%Y-%m-%d 00:00:00',"purchase_time", 'weekday 1')) AS "quantised_weeks"
+  FROM "transactions"
+LEFT JOIN "transaction_category" ON "transactions.id" = "transaction_category.transaction_id"
+/);
+
+1;
