005388b27e28255dee8db3a2b12d0ceddc2f7e84
Finn <toyou1995@gmail.com>
2018-01-17 17:14:50 +0000

    fixed api and quantised statement code

--- lib/Pear/LocalLoop/Schema/Result/ViewQuantisedTransactionCategorySQLite.pm
+++ lib/Pear/LocalLoop/Schema/Result/ViewQuantisedTransactionCategorySQLite.pm
@@ -10,15 +10,15 @@ __PACKAGE__->table('view_quantised_transactions');
 __PACKAGE__->result_source_instance->is_virtual(1);
 
 __PACKAGE__->result_source_instance->view_definition( qq/
-SELECT "value",
-       "distance",
-       "purchase_time",
-       "buyer_id",
-       "seller_id",
+SELECT "transactions.value",
+       "transactions.distance",
+       "transactions.purchase_time",
+       "transactions.buyer_id",
+       "transactions.seller_id",
        "transaction_category.category_id",
-       DATETIME(STRFTIME('%Y-%m-%d %H:00:00',"purchase_time")) AS "quantised_hours",
-       DATETIME(STRFTIME('%Y-%m-%d 00:00:00',"purchase_time")) AS "quantised_days",
-       DATETIME(STRFTIME('%Y-%m-%d 00:00:00',"purchase_time", 'weekday 1')) AS "quantised_weeks"
+       DATETIME(STRFTIME('%Y-%m-%d %H:00:00',"transactions.purchase_time")) AS "quantised_hours",
+       DATETIME(STRFTIME('%Y-%m-%d 00:00:00',"transactions.purchase_time")) AS "quantised_days",
+       DATETIME(STRFTIME('%Y-%m-%d 00:00:00',"transactions.purchase_time", 'weekday 1')) AS "quantised_weeks"
   FROM "transactions"
 LEFT JOIN "transaction_category" ON "transactions.id" = "transaction_category.transaction_id"
 /);
