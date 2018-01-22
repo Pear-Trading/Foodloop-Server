6cd7df12592a21704b8965cfa15ed6aea637dad5
Finn <toyou1995@gmail.com>
2018-01-22 16:26:45 +0000

    working API with arrays

--- lib/Pear/LocalLoop/Schema/Result/ViewQuantisedTransactionCategorySQLite.pm
+++ lib/Pear/LocalLoop/Schema/Result/ViewQuantisedTransactionCategorySQLite.pm
@@ -10,17 +10,17 @@ __PACKAGE__->table('view_quantised_transactions');
 __PACKAGE__->result_source_instance->is_virtual(1);
 
 __PACKAGE__->result_source_instance->view_definition( qq/
-SELECT "transactions.value",
-       "transactions.distance",
-       "transactions.purchase_time",
-       "transactions.buyer_id",
-       "transactions.seller_id",
-       "transaction_category.category_id",
-       DATETIME(STRFTIME('%Y-%m-%d %H:00:00',"transactions.purchase_time")) AS "quantised_hours",
-       DATETIME(STRFTIME('%Y-%m-%d 00:00:00',"transactions.purchase_time")) AS "quantised_days",
-       DATETIME(STRFTIME('%Y-%m-%d 00:00:00',"transactions.purchase_time", 'weekday 1')) AS "quantised_weeks"
+SELECT "transactions"."value",
+       "transactions"."distance",
+       "transactions"."purchase_time",
+       "transactions"."buyer_id",
+       "transactions"."seller_id",
+       "transaction_category"."category_id",
+       DATETIME(STRFTIME('%Y-%m-%d %H:00:00',"transactions"."purchase_time")) AS "quantised_hours",
+       DATETIME(STRFTIME('%Y-%m-%d 00:00:00',"transactions"."purchase_time")) AS "quantised_days",
+       DATETIME(STRFTIME('%Y-%m-%d 00:00:00',"transactions"."purchase_time", 'weekday 1')) AS "quantised_weeks"
   FROM "transactions"
-LEFT JOIN "transaction_category" ON "transactions.id" = "transaction_category.transaction_id"
+LEFT JOIN "transaction_category" ON "transactions"."id" = "transaction_category"."transaction_id"
 /);
 
 1;
