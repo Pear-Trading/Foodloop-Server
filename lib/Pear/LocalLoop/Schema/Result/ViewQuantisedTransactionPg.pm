4ebe99f6adda59d7c986815d6fa0aa0477b1973f
Finn <toyou1995@gmail.com>
2018-03-02 16:42:30 +0000

    removed debug line and unneeded code

--- lib/Pear/LocalLoop/Schema/Result/ViewQuantisedTransactionPg.pm
+++ lib/Pear/LocalLoop/Schema/Result/ViewQuantisedTransactionPg.pm
@@ -15,7 +15,6 @@ SELECT "value",
        "purchase_time",
        "buyer_id",
        "seller_id",
-       "essential",
        DATE_TRUNC('hour', "purchase_time") AS "quantised_hours",
        DATE_TRUNC('day', "purchase_time") AS "quantised_days",
        DATE_TRUNC('week', "purchase_time") AS "quantised_weeks"
