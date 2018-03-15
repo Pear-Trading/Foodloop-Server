60438ea51c4a6781e076bd8043153842b31db451
Finn <toyou1995@gmail.com>
2018-03-15 13:06:50 +0000

    amended transactionlist code

--- lib/Pear/LocalLoop/Controller/Api/Transactions.pm
+++ lib/Pear/LocalLoop/Controller/Api/Transactions.pm
@@ -42,16 +42,16 @@ sub post_transaction_list_purchases {
       purchase_time => $c->format_iso_datetime($_->purchase_time),
     }} $transactions->all
   );
-
+  
   my @recurring_transaction_list = (
     map {{
       id => $_->id,
       seller => $_->seller->name,
       value => $_->value / 100000,
       start_time => $c->format_iso_datetime($_->start_time),
-      last_updated => $c->format_iso_datetime($_->last_updated),
+      last_updated => $c->format_iso_datetime($_->last_updated) || undef,
       essential => $_->essential,
-      category => ( defined $_->category ? $_->category->name : 'Uncategorised' ),
+      category => $_->category_id,
       recurring_period => $_->recurring_period,
     }} $recurring_transactions->all
   );
