adb19e84f78fd78e5b4feeb5799f661d65cec9ff
Finn <toyou1995@gmail.com>
2018-06-18 12:10:08 +0100

    added default

--- lib/Pear/LocalLoop/Controller/Api/Stats.pm
+++ lib/Pear/LocalLoop/Controller/Api/Stats.pm
@@ -133,7 +133,7 @@ sub post_customer {
   }
 
   my @cat_lists = map { { category => $_, value => $cat_total_list{$_},
-  icon => $c->schema->resultset('Category')->as_hash_name_icon->{$_} } } sort keys %cat_total_list;
+  icon => $c->schema->resultset('Category')->as_hash_name_icon->{$_} || 'question'} } sort keys %cat_total_list;
   $data->{cat_list} = [ sort { $b->{value} <=> $a->{value} } @cat_lists ];
 
   my $purchase_no_essential_rs = $purchase_rs->search({
