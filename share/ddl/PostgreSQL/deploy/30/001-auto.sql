6d71d51581165f56a8c19286c89737d3e46d35f6
Ben Goldsworthy <b.r.goldsworthy@lancaster.ac.uk>
2020-10-14 13:40:41 +0100

    Commit database files

--- share/ddl/PostgreSQL/deploy/30/001-auto.sql
+++ share/ddl/PostgreSQL/deploy/30/001-auto.sql
@@ -1,7 +1,7 @@
--- 
+--
 -- Created by SQL::Translator::Producer::PostgreSQL
--- Created on Mon Sep  2 13:06:08 2019
--- 
+-- Created on Tue Aug 25 15:48:42 2020
+--
 ;
 --
 -- Table: account_tokens
